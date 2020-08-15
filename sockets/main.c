a5eca172f4c9ff2dc6a06c356c178cd28631f000
lacadev <36073068+lacadev@users.noreply.github.com>
2020-08-15 18:18:26 +0200

    Connect socket to a server

--- sockets/main.c
+++ sockets/main.c
@@ -1,23 +1,70 @@
 /* Implementation of the tutorial: */
 /* https://www.binarytides.com/socket-programming-c-linux-tutorial/ */
 
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  int socket_desc;
-  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+/* Structs being used internally
 
-  if (socket_desc == -1) {
-    close(socket_desc);
+//IPv4 AF_INET sockets
+struct sockaddr_in {
+  // Protocol (e.g. AF_INET, AF_INET6)
+  short sin_family;
+  // Port in network byte order (e.g htons(80))
+  // See https://stackoverflow.com/questions/19207745/htons-function-in-socket-programing
+  unsigned short sin_port;
+  // See struct below
+  struct in_addr sin_addr;
+  char sin_zero[8];
+};
+
+struct in_addr {
+  // IP address in long format
+  // load with inet_pton(), which  converts an IP address to a long format
+  unsigned long s_addr;
+};
+
+struct sockaddr {
+  // Address family, AF_XXX
+  unsigned short sa_family;
+  // 14 bytes of protocol
+  char sa_data[14];
+};
+
+*/
+
+int main(int argc, char *argv[]) {
+  int socket_descriptor;
+  struct sockaddr_in server;
+
+  // Create socket
+  // AF_INET means we're using IPv4
+  // SOCK_STREAM means we want to use the TCP protocol
+  socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_descriptor == -1) {
+    close(socket_descriptor);
     printf("Could not create socket");
     exit(EXIT_FAILURE);
   }
-
   printf("Socket created successfully!\n");
-  close(socket_desc);
+
+  server.sin_addr.s_addr = inet_addr("1.1.1.1");
+  server.sin_family = AF_INET;
+  server.sin_port = htons(80);
+
+  // Connect to server
+  if (connect(socket_descriptor, (struct sockaddr *)& server, sizeof(server)) < 0) {
+    close(socket_descriptor);
+    printf("Failed to connect to server\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Connect to server successfully!\n");
+
+
+  close(socket_descriptor);
 
   return 0;
 }
